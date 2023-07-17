64a007d22f40f038636397a4114a00cdf645e7f0
prathibha <prathibha@LAPTOP-NAAMUCTV>
2023-07-17 11:55:15 +0530

    12 testcases passed

--- javascript/task.js
+++ javascript/task.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 
-const taskFilePath = "./task.txt";
-const completedFilePath = "./completed.txt";
+const taskFilePath = `${__dirname}/task.txt`;
+const completedFilePath = `${__dirname}/completed.txt`;
 
 const addTask = (priority, description) => {
   const task = `${priority} ${description}`;
@@ -19,8 +19,8 @@ const listTasks = () => {
     if (tasks.length > 0) {
       tasks.sort((a, b) => a[0] - b[0]);
       console.log("Tasks:");
-      tasks.forEach(([priority, description], index) => {
-        console.log(`${index + 1}. ${description} [${priority}]`);
+      tasks.forEach(([priority, ...description], index) => {
+        console.log(`${index + 1}. ${description.join(" ")} [${priority}]`);
       });
     } else {
       console.log("There are no pending tasks!");
@@ -38,7 +38,9 @@ const deleteTask = (index) => {
       fs.writeFileSync(taskFilePath, tasks.join("\n") + "\n");
       console.log(`Deleted task #${index + 1}`);
     } else {
-      console.log(`Error: task with index #${index + 1} does not exist. Nothing deleted.`);
+      console.log(
+        `Error: task with index #${index + 1} does not exist. Nothing deleted.`
+      );
     }
   } else {
     console.log("There are no pending tasks!");
@@ -55,7 +57,9 @@ const markTaskAsDone = (index) => {
       fs.appendFileSync(completedFilePath, task.split(" ").slice(1).join(" ") + "\n");
       console.log("Marked item as done.");
     } else {
-      console.log(`Error: no incomplete item with index #${index + 1} exists.`);
+      console.log(
+        `Error: no incomplete item with index #${index + 1} exists.`
+      );
     }
   } else {
     console.log("There are no pending tasks!");
@@ -73,8 +77,8 @@ const reportTasks = () => {
 
     console.log(`Pending: ${pendingTasks.length}`);
     pendingTasks.forEach((task, index) => {
-      const [priority, description] = task.split(" ");
-      console.log(`${index + 1}. ${description} [${priority}]`);
+      const [priority, ...description] = task.split(" ");
+      console.log(`${index + 1}. ${description.join(" ")} [${priority}]`);
     });
 
     console.log(`\nCompleted: ${completedTasks.length}`);
@@ -99,11 +103,11 @@ switch (command) {
     }
     break;
 
-  case "list":
+  case "ls":
     listTasks();
     break;
 
-  case "delete":
+  case "del":
     const deleteIndex = parseInt(process.argv[3]) - 1;
     if (!isNaN(deleteIndex)) {
       deleteTask(deleteIndex);
@@ -124,8 +128,8 @@ switch (command) {
   case "help":
     console.log(`Usage :-
 $ ./task add [priority] "[description]"    # Add a new item with priority [priority] and text "[description]" to the list
-$ ./task list                             # Show incomplete priority list items sorted by priority in ascending order
-$ ./task delete [index]                   # Delete the incomplete item with the given [index]
+$ ./task ls                               # Show incomplete priority list items sorted by priority in ascending order
+$ ./task del [index]                      # Delete the incomplete item with the given [index]
 $ ./task done [index]                     # Mark the incomplete item with the given [index] as complete
 $ ./task help                             # Show usage
 $ ./task report                           # Generate a report`);
