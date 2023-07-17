9717d40f3d43ffd38664d64f99f9fab4c57e4103
prathibha <prathibha@LAPTOP-NAAMUCTV>
2023-07-17 13:39:40 +0530

    prints help when no additional args are provided

--- javascript/task.js
+++ javascript/task.js
@@ -142,12 +142,12 @@ $ ./task report               # Statistics`);
 
   default:
     const usage = `Usage :-
-    $ ./task add 2 hello world    # Add a new item with priority 2 and text "hello world" to the list
-    $ ./task ls                   # Show incomplete priority list items sorted by priority in ascending order
-    $ ./task del INDEX            # Delete the incomplete item with the given index
-    $ ./task done INDEX           # Mark the incomplete item with the given index as complete
-    $ ./task help                 # Show usage
-    $ ./task report               # Statistics`;
+$ ./task add 2 hello world    # Add a new item with priority 2 and text "hello world" to the list
+$ ./task ls                   # Show incomplete priority list items sorted by priority in ascending order
+$ ./task del INDEX            # Delete the incomplete item with the given index
+$ ./task done INDEX           # Mark the incomplete item with the given index as complete
+$ ./task help                 # Show usage
+$ ./task report               # Statistics`;
     console.log(usage);
     break;
 }
