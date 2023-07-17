1635ce10c78d8d1ae8051eba7817f0870b31e835
prathibha <prathibha@LAPTOP-NAAMUCTV>
2023-07-17 13:38:17 +0530

    indentation correction for prints help

--- javascript/task.js
+++ javascript/task.js
@@ -127,13 +127,13 @@ switch (command) {
     break;
 
   case "help":
-    console.log(`Usage:
-$ ./task add 2 "hello world"    # Add a new item with priority 2 and text "hello world" to the list
-$ ./task ls                      # Show incomplete priority list items sorted by priority in ascending order
-$ ./task del INDEX               # Delete the incomplete item with the given index
-$ ./task done INDEX              # Mark the incomplete item with the given index as complete
-$ ./task help                    # Show usage
-$ ./task report                  # Statistics`);
+    console.log(`Usage :-
+$ ./task add 2 hello world    # Add a new item with priority 2 and text "hello world" to the list
+$ ./task ls                   # Show incomplete priority list items sorted by priority in ascending order
+$ ./task del INDEX            # Delete the incomplete item with the given index
+$ ./task done INDEX           # Mark the incomplete item with the given index as complete
+$ ./task help                 # Show usage
+$ ./task report               # Statistics`);
     break;
 
   case "report":
@@ -141,13 +141,13 @@ $ ./task report                  # Statistics`);
     break;
 
   default:
-    const usage = `Usage:
-$ ./task add 2 "hello world"     # Add a new item with priority 2 and text "hello world" to the list
-$ ./task ls                      # Show incomplete priority list items sorted by priority in ascending order
-$ ./task del INDEX               # Delete the incomplete item with the given index
-$ ./task done INDEX              # Mark the incomplete item with the given index as complete
-$ ./task help                    # Show usage
-$ ./task report                  # Statistics`;
+    const usage = `Usage :-
+    $ ./task add 2 hello world    # Add a new item with priority 2 and text "hello world" to the list
+    $ ./task ls                   # Show incomplete priority list items sorted by priority in ascending order
+    $ ./task del INDEX            # Delete the incomplete item with the given index
+    $ ./task done INDEX           # Mark the incomplete item with the given index as complete
+    $ ./task help                 # Show usage
+    $ ./task report               # Statistics`;
     console.log(usage);
     break;
 }
